a7ec9b575de44991b2fae376c3099ffe63858e38
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-02-28 20:04:44 -0500

    added resetGame() function to automatically randomize the values of the gameNumber and each crystal upon the start of a new game

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Crystals Collector
 __Game Instructions__
 * The player is given a random number at the start of the game.
-* The player will see four crystals. The points value of each crystal is hidden until it is clicked on.
+* There are five crystals at the bottom of the page. The points value of each crystal is hidden.
 * By clicking on a crystal, the player will add will add that crystal's amount of points to the total score.
 * The goal is for the player to match the total score to the random number provided at the beginning of the game.
 * If the total score matches the random number, the player wins.
 * If the total score goes above the random number, the player loses.
-* Each time a new game starts, the game will generate a new random number and the points value of each crystal will change.
+* Each time a new game starts, the game automatically generates a new random number and the points value of each crystal changes.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,34 @@
 
+// <--- Global Variables --->
 var randomNumber = function getRandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-var crystals = {
-    "1": randomNumber(1,12),
-    "2": randomNumber(1,12),
-    "3": randomNumber(1,12),
-    "4": randomNumber(1,12),
-    "5": randomNumber(1,12)
-}
-var gameNumber = randomNumber(19,120);
-var totalScore = 0;
+var crystals;
+var gameNumber;
+var totalScore;
 var wins = 0;
 var losses = 0;
-// var userWon = false;
-// var userLost = false;
 
+// <--- Global Function --->
+//resets game by assigning new random numbers to the gameNumber and each crystal and resetss totalScore to 0
+function resetGame() {
+    crystals = {
+        "1": randomNumber(1,12),
+        "2": randomNumber(1,12),
+        "3": randomNumber(1,12),
+        "4": randomNumber(1,12),
+        "5": randomNumber(1,12)
+    }
+    console.log(crystals);
+    gameNumber = randomNumber(19,120);
+    $(".game-number").text(gameNumber);
+    totalScore = 0;
+    $(".total-score").text(totalScore);
+}
+
+// <--- Start Game --->
 //shows instructions when clicked
 $(".instructions-button").on("click", function() {
     $(".instructions-button").hide();
@@ -25,32 +36,34 @@ $(".instructions-button").on("click", function() {
 })
 $(".collapse-button").on("click", function() {
     $(".instructions").slideUp();
-    $(".instructions-button").show("slow");
+    $(".instructions-button").fadeIn(2000);
 })
 
+resetGame();
+
 //shows initial scoreboard
-$(".game-number").text(gameNumber);
-$(".total-score").text(totalScore);
 $(".wins").text(wins);
 $(".losses").text(losses);
 
-console.log(crystals);
-
+//Loops through crystals object. Upon the click of a crystal image, when i equals the html id value of the image, add the value of i to totalScore
 $.each(crystals, function(i, val) {
     $("#" + i + "").on("click", function() {
         totalScore = totalScore + val;
         $(".total-score").text(totalScore);
         if (totalScore === gameNumber) {
-            $(".you-win").fadeIn("slow").fadeOut("slow");
+            $(".you-won").fadeIn("slow").fadeOut("slow");
             wins++;
             $(".wins").text(wins);
+            resetGame();
         } else if (totalScore > gameNumber) {
-            $(".you-lose").fadeIn("slow").fadeOut("slow");
+            $(".you-lost").fadeIn("slow").fadeOut("slow");
             losses++;
             $(".losses").text(losses);
+            resetGame();
         }
     })
 }) 
+
 
 
 
